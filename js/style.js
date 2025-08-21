89d917fb8aa0922d6975bfb90a0d1cd6aa30ad80
Joshua <ojdaraakinsanya@gmail.com>
2025-08-21 22:40:28 +0100

    fix(js): add toggle side menu functionality:
    Adds ability to toggle side menu visibility for mobile

--- js/style.js
+++ js/style.js
@@ -2,7 +2,16 @@
 
 const closeButton = document.querySelector("#close-menu-button");
 const openButton = document.querySelector("#open-menu-button");
+const navmenu = document.querySelector("#nav-menu");
 
+closeButton.addEventListener('click', function () {
+    if(window.innerWidth < 1024) {
+        navmenu.style.display = 'none';
+    }
+})
 
+openButton.addEventListener('click', function () {
+    navmenu.style.display = 'flex';
+})
 
 })();
