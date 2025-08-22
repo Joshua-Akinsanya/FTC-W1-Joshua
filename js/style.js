4b83058902a81d96b98cee563b29db48251e85a0
Joshua <ojdaraakinsanya@gmail.com>
2025-08-22 08:58:06 +0100

    fix(css/js): adjust menu position on desktop UI
    Using the transform property on the menu caused it to shift unexpectedly
    to the right in desktop UI. This change fixes that

--- js/style.js
+++ js/style.js
@@ -5,11 +5,26 @@ const openButton = document.querySelector("#open-menu-button");
 const navmenu = document.querySelector("#nav-menu");
 
 closeButton.addEventListener('click', function () {
-    navmenu.style.transform = 'translateX(100%)'
+    navmenu.style.transform = 'translateX(100%)';
+    setTimeout( () => {
+        navmenu.style.transition = 'none';
+    }, 300);
 })
 
 openButton.addEventListener('click', function () {
+    navmenu.style.transition = 'transform 0.3s ease';
     navmenu.style.transform = 'translateX(0)';
+})
+
+// Prevent navmenu from shifting unexpectedly when resizing window
+
+window.addEventListener('resize', function () {
+    if(this.innerWidth >= 1024) {
+        navmenu.style.transform = 'translateX(0)';
+    }
+    else {
+        navmenu.style.transform = 'translateX(100%)';
+    }
 })
 
 })();
